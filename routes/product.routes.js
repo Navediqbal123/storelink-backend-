bf138b82671e926df636831ee494e19b15ee9f12
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-13 23:43:56 +0530

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
+import auth from "../middleware/auth.js";
 import shopkeeperOnly from "../middleware/shopkeeper.js";
 import { addProduct, getProducts } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-router.post("/add-product", shopkeeperOnly, addProduct);
-router.get("/products", getProducts);
+// Add product (only logged-in shopkeeper)
+router.post("/add", auth, shopkeeperOnly, addProduct);
+
+// Get all products (public)
+router.get("/", getProducts);
 
 export default router;
