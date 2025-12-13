b99d91be685efdd7ba85039205da8121011c656a
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-13 19:58:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import shopkeeperRoutes from "./routes/shopkeeper.routes.js";
+import productRoutes from "./routes/product.routes.js";
+
+dotenv.config();
 
 const app = express();
+
+// middlewares
+app.use(cors());
 app.use(express.json());
 
+// test route
+app.get("/", (req, res) => {
+  res.send("Storelink Backend Running ✅");
+});
+
+// routes
 app.use("/auth", authRoutes);
+app.use("/shopkeeper", shopkeeperRoutes);
+app.use("/products", productRoutes);
 
-app.get("/", (req, res) => res.send("API running"));
-
+// server start
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server started"));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
