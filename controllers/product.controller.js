e29a3f5bd3bdf1265a1570ce53de0d2525638e0e
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-13 21:55:11 +0530

    Create product.controller.js

---
+++ controllers/product.controller.js
@@ -0,0 +1,7 @@
+export const addProduct = (req, res) => {
+  res.json({ message: "Product added (dummy)" });
+};
+
+export const getProducts = (req, res) => {
+  res.json({ message: "Products fetched (dummy)" });
+};
