ed7e839db67a211aba0c03c32f4091cb0100e6f6
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-13 08:34:18 +0530

    Add product routes for adding and retrieving products

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import shopkeeperOnly from "../middleware/shopkeeper.js";
+import { addProduct, getProducts } from "../controllers/productController.js";
 
+const router = express.Router();
+
+router.post("/add-product", shopkeeperOnly, addProduct);
+router.get("/products", getProducts);
+
+export default router;
