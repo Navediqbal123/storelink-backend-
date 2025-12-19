23d2ef4176550ee4d712cd5c9136c6a6cb4a9090
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-19 15:06:16 +0530

    Update admin.controller.js

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,1 +1,9 @@
+export const adminStats = async (req, res) => {
+  const users = await req.supabase.from("profiles").select("id", { count: "exact" });
+  const products = await req.supabase.from("products").select("id", { count: "exact" });
 
+  res.json({
+    totalUsers: users.count,
+    totalProducts: products.count
+  });
+};
